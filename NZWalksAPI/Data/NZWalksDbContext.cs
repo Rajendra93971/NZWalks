8cbc4eb51cc8b4378c405998eb259f659f98f753
ManvendraDeveloper <manvendra.pandey2016@gmail.com>
2026-02-02 19:21:07 +0530

    Db Connected

---
+++ NZWalksAPI/Data/NZWalksDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using NZWalksAPI.Models;
+
+namespace NZWalksAPI.Data
+{
+    public class NZWalksDbContext : DbContext
+    {
+        public NZWalksDbContext(DbContextOtions dbContexoptions) : base(dbContexoptions)
+        {
+                
+        }
+
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Region> Regions { get; set; }
+
+        public DbSet<Walk> Walks { get; set; }
+
+    }
+}
